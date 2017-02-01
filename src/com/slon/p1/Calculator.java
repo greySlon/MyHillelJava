aae6fef3f183582e6660fb68d06e47da41cd4d29
GreySlon <garanenkosergii@gmail.com>
2017-02-02 00:27:32 +0200

    Task 2 - calculator version 2

--- src/com/slon/p1/Calculator.java
+++ src/com/slon/p1/Calculator.java
@@ -12,6 +12,28 @@ import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Calculator {
+    static boolean exit(){
+        BufferedReader br=new BufferedReader(new InputStreamReader(in));
+
+        while (true) {
+            out.print("Would you like to continue? Y/N");
+            try {
+                switch (br.readLine().toLowerCase()){
+                    case "y":
+                        return false;
+                    case "n":
+                        return true;
+                    default:
+                        out.println("I don't get you. So let's start again.");
+                        continue;
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            out.println();
+        }
+    }
+
     private float customNum1;
     private float customNum2;
     private BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -26,13 +48,51 @@ public class Calculator {
         out.println(String.format("Your input %-8.3f as a second number.", customNum2));
     }
 
-    public void showResult() {
+    public void setOperation() throws IOException{
+        out.print("Enter operation type(+,-,/,*)");
+        String operationType = reader.readLine();
         out.println();
+        out.println(String.format("Calculator Task 2(result of \"%s\" operations)", operationType));
+        switch (operationType){
+            case "+":
+                sum();
+                break;
+            case "-":
+                difference();
+                break;
+            case "/":
+                division();
+                break;
+            case "*":
+                multiplication();
+                break;
+                default:
+                    out.println(String.format("\n%s isn't supported operation\n", operationType));
+                    throw new NumberFormatException();
+        }
+    }
+
+    public void showResultTask1() {
+        out.println();
+        out.print("Calculator Task 1(all operations results)");
+        sum();
+        difference();
+        multiplication();
+        division();
+        out.println();
+    }
+
+    private void sum(){
         out.println(String.format("Sum is %-8.3f", customNum1 + customNum2));
+    }
+    private void difference(){
         out.println(String.format("Difference is %-8.3f", customNum1 - customNum2));
+    }
+    private void multiplication(){
         out.println(String.format("Multiplication is %-8.3f", customNum1 * customNum2));
+    }
+    private void division(){
         out.println(String.format("Division is %-8.3f", customNum1 / customNum2));
-        out.println();
     }
 
     public void reset() {
@@ -59,36 +119,16 @@ public class Calculator {
             try {
                 calculator.setCustomNum1();
                 calculator.setCustomNum2();
-                calculator.showResult();
+                calculator.setOperation();
+                calculator.showResultTask1();
                 calculator.reset();
             } catch (IOException e) {
                 out.println("Something's wrong");
                 return;
             }catch (NumberFormatException e){
-               if(isStop()) break;
+               if(exit()) break;
             }
         }
     }
 
-    static boolean isStop(){
-        BufferedReader br=new BufferedReader(new InputStreamReader(in));
-
-        while (true) {
-            out.print("Would you like to continue? Y/N");
-            try {
-                switch (br.readLine().toLowerCase()){
-                    case "y":
-                        return false;
-                    case "n":
-                        return true;
-                        default:
-                            out.println("I don't get you. So let's start again.");
-                            continue;
-                }
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            out.println();
-        }
-    }
 }
