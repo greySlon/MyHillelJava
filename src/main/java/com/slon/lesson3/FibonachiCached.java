5d517ea35b2128b21573b8dd54942f86ddf11071
GreySlon <garanenkosergii@gmail.com>
2017-02-08 21:05:13 +0200

    fibonachi (BigInteger)

--- src/main/java/com/slon/lesson3/FibonachiCached.java
+++ src/main/java/com/slon/lesson3/FibonachiCached.java
@@ -3,6 +3,7 @@ package com.slon.lesson3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,19 +11,19 @@ import java.util.Map;
  * Created by Sergii on 03.02.2017.
  */
 public class FibonachiCached {
-    private Map<Integer, Long> cache = new HashMap<>();
+    private Map<Integer, BigInteger> cache = new HashMap<>();
 
-    public long getItemRequrs(int x) {
+    public BigInteger getItemRequrs(int x) {
         if (x == 0 || x < 0) {
-            return 0;
+            return new BigInteger("0");
         }
         if (x == 1) {
-            return 1;
+            return new BigInteger("1");
         } else {
             if (cache.containsKey(x)) {
                 return cache.get(x);
             } else {
-                long result = getItemRequrs(x - 1) + getItemRequrs(x - 2);
+                BigInteger result =getItemRequrs(x - 1).add(getItemRequrs(x - 2));
                 cache.put(x, result);
                 return result;
             }
@@ -30,16 +31,16 @@ public class FibonachiCached {
     }
 
 
-    public long getItem(int x) {
-        long res = 0, n_1 = 1, n_2 = 0;
+    public BigInteger getItem(int x) {
+        BigInteger res = new BigInteger("0"), n_1 = new BigInteger("1"), n_2 = new BigInteger("0");
         if (x == 0 || x < 0) {
-            return 0;
+            return new BigInteger("0");
         }
         if (x == 1) {
-            return 1;
+            return new BigInteger("1");
         } else {
             for (int i = 1; i < x; i++) {
-                res = n_1 + n_2;
+                res = n_1.add(n_2);
                 n_2=n_1;
                 n_1=res;
             }
@@ -58,6 +59,9 @@ public class FibonachiCached {
                 System.out.print("Input a number & you'll get its fibonachi(input not number to EXIT): ");
                 System.out.println();
                 int x = Integer.parseInt(reader.readLine());
+//                for (int i=0;i<130;i++) {
+//                    System.out.println(String.format("%d - %d",i,fibonachi.getItemRequrs(i)));
+//                }
                 System.out.println(fibonachi.getItemRequrs(x));
                 System.out.println(fibonachi.getItem(x));
             } catch (NumberFormatException e) {
