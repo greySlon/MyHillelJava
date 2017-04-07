5f9d18de380e8bf7a68d29b734fd449b4da9ae4e
GreySlon <garanenkosergii@gmail.com>
2017-04-07 13:14:18 +0300

    speedtest any method  using reflection

--- src/main/java/com/slon/io/IOSpeedTest.java
+++ src/main/java/com/slon/io/IOSpeedTest.java
@@ -2,6 +2,7 @@ package com.slon.io;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,18 +19,38 @@ public class IOSpeedTest {
         return timeUnit.convert(stopTime - startTime, TimeUnit.NANOSECONDS);
     }
 
-    public String getDateFromConsole() throws Exception{
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+    public long timeTest(Object target, String methodName, TimeUnit timeUnit, Object... args) throws Exception {
+        Object result = null;
+        Class[] paramTypes = new Class[args.length];
+        for (int i = 0; i < args.length; i++) {
+            paramTypes[i] = args[i].getClass();
+        }
+        Method method = target.getClass().getMethod(methodName, paramTypes);
+
+        startTime = System.nanoTime();
+        result = method.invoke(target, args);
+        stopTime = System.nanoTime();
+        return timeUnit.convert(stopTime - startTime, TimeUnit.NANOSECONDS);
+    }
+
+    public String getDateFromConsole() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
     }
+
     public static void main(String[] args) throws Exception {
         IOSpeedTest speedTest = new IOSpeedTest();
         UnbufferdeIO unbufferdeIO = new UnbufferdeIO();
         BufferedIO bufferedIO = new BufferedIO();
 
-        String sourceFileName=speedTest.getDateFromConsole();
-        String destinationFileName=speedTest.getDateFromConsole();
+        String sourceFileName = speedTest.getDateFromConsole();
+        String destinationFileName = speedTest.getDateFromConsole();
 
+        //reflection - any method invocation
+        System.out.println(speedTest.timeTest(bufferedIO, "copyTo", TimeUnit.MILLISECONDS, sourceFileName, destinationFileName));
+        System.out.println(speedTest.timeTest(unbufferdeIO, "copyTo", TimeUnit.MILLISECONDS, sourceFileName, destinationFileName));
+
+        //
         System.out.println(speedTest.timeTest(((Copieble) unbufferdeIO), TimeUnit.MILLISECONDS, sourceFileName, destinationFileName));
         System.out.println(speedTest.timeTest(((Copieble) bufferedIO), TimeUnit.MILLISECONDS, sourceFileName, destinationFileName));
     }
